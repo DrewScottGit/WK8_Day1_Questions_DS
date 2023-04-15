bd0ca891768fe33a1aeca3a916fae29417a4cff7
Drew S <djscott37@gmail.com>
2023-04-15 13:34:07 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+How do you make a Circle in CSS?
+create a div with a height and width that are the same. Then add a border radius and set it to 50%
+What is Z-index?
+A property that defines the order of elemtents. This determins elemtents in layers.
+What is a Vendor Prefix and What is it's Purpose?
+A way for browsers to give access to new CSS features not yet stable
+What is the difference between CSS Grid and Flexbox?
+flexbox places elements in rows and columns. Flexbox is one dimensional. Grid is built for two dimensional layout. Takes rows and columns into scope at the same time.
+How might you Speed Up a Slow App? hint Google Lighthouse in Chrome Developer Tools....
+reduce the number of HTTP requests. Optimize image sizes. Use Lighthouse to audit web pages for inefficiencies
+What is CRUD?
+Create, Read, Update, and Delete
+What is OOP?
+Programing model that organizes software design around data, or objects, rather than functions and logic
